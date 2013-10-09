94c298517776ee078864ce0a61c978dfe5c6c481
Justin Lambert <jlambert@eml.cc>
2013-10-09 15:28:55 -0600

    add spec tests for tls true/false

--- spec/classes/postfix_install_spec.rb
+++ spec/classes/postfix_install_spec.rb
@@ -2,14 +2,29 @@ require 'spec_helper'
 
 describe 'postfix::install', :type => :class do
   let(:title) { 'postfix' }
-  let(:params) { {
-    :tls         => true,
-    :tls_package => 'openssl',
-  } }
 
-  it { should create_class('postfix::install') }
-  it { should contain_package('sendmail').with_ensure('absent') }
-  it { should contain_package('postfix').with_ensure('latest') }
-  it { should contain_package('openssl').with_ensure('latest') }
+  describe 'default' do
+    let(:params) { {
+      :tls         => false,
+      :tls_package => 'openssl'
+    } }
+
+    it { should create_class('postfix::install') }
+    it { should contain_package('sendmail').with_ensure('absent') }
+    it { should contain_package('postfix').with_ensure('latest') }
+    it { should_not contain_package('openssl') }
+  end
+
+  describe 'with ssl' do
+    let(:params) { {
+      :tls         => true,
+      :tls_package => 'openssl',
+    } }
+
+    it { should create_class('postfix::install') }
+    it { should contain_package('sendmail').with_ensure('absent') }
+    it { should contain_package('postfix').with_ensure('latest') }
+    it { should contain_package('openssl').with_ensure('latest') }
+  end
 
 end
