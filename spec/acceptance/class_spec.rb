bd0143497342ab4bfacda3681c5c84e1cccd39d6
Justin Lambert <jlambert@eml.cc>
2014-12-12 19:12:31 -0700

    add inital acceptance tests

---
+++ spec/acceptance/class_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper_acceptance'
+
+describe 'postfix classes' do
+
+  context 'defaults' do
+    it 'should work idempotently with no errors' do
+      pp = <<-EOS
+      class { 'postfix': }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes  => true)
+    end
+
+    describe package('sendmail') do
+      it { is_expected.not_to be_installed }
+    end
+
+    describe package('postfix') do
+      it { is_expected.to be_installed }
+    end
+
+    describe service('postfix') do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
+    end
+
+    describe port(25) do
+      it { should be_listening }
+    end
+
+  end
+
+end
